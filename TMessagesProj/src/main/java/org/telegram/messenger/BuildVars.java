34b69906acb6fdb79158233179013bae586cc58f
tetodevaseel-bit <tetodevaseel@gmail.com>
2026-06-04 01:05:41 +0300

    Set Shoof TV API credentials

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -28,8 +28,8 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
 
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 11704522;
+    public static String APP_HASH = "4d8519ffb0302f8ac42574a44839bde3";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "";
